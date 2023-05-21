50b9236cb1f85d620bce3b68437ccf215a7090db
deanbred <deanbred@gmail.com>
2023-05-21 06:14:51 -0400

    added raffle

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,15 +1,39 @@
+const { ethers } = require("hardhat")
+
 const networkConfig = {
+  default: {
+    name: "hardhat",
+    keepersUpdateInterval: "30",
+  },
   31337: {
     name: "localhost",
+    subscriptionId: "588",
+    gasLane:
+      "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c", // 30 gwei
+    keepersUpdateInterval: "30",
+    raffleEntranceFee: ethers.utils.parseEther("0.01"), // 0.01 ETH
+    callbackGasLimit: "500000", // 500,000 gas
   },
-  // Price Feed Address, values can be obtained at https://docs.chain.link/data-feeds/price-feeds/addresses
   11155111: {
-    name: "goerli",
-    ethUsdPriceFeed: "0x694AA1769357215DE4FAC081bf1f309aDC325306",
+    name: "sepolia",
+    subscriptionId: "6926",
+    gasLane:
+      "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c", // 30 gwei
+    keepersUpdateInterval: "30",
+    raffleEntranceFee: ethers.utils.parseEther("0.01"), // 0.01 ETH
+    callbackGasLimit: "500000", // 500,000 gas
+    vrfCoordinatorV2: "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625",
+  },
+  1: {
+    name: "mainnet",
+    keepersUpdateInterval: "30",
   },
 }
+
 // Fibonacci Sequence 1.12 Billion tokens
 const INITIAL_SUPPLY = ethers.BigNumber.from("1123581321000000000000000000")
+
+const VERIFICATION_BLOCK_CONFIRMATIONS = 6
 
 const developmentChains = [
   "hardhat",
@@ -22,6 +46,7 @@ const developmentChains = [
 
 module.exports = {
   networkConfig,
+  VERIFICATION_BLOCK_CONFIRMATIONS,
   developmentChains,
   INITIAL_SUPPLY,
 }
